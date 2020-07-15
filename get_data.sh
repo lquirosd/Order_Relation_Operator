f63064b4160b8c435b1680162b69941531694574
lquirosd <lorenzoqd@gmail.com>
2020-07-15 10:23:37 +0200

    + automatic download FCR dataset

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+if [ 3 == 4 ]; then
 #-----------------------------------------------------------------------------#
 #---- Get OHG dataset
 #-----------------------------------------------------------------------------#
@@ -31,33 +31,39 @@ for f in $(<../test.lst); do
 done
 
 cd ../../..
+fi
 #-----------------------------------------------------------------------------#
 #---- FCR dataset has to be requested to the owners, hence no automatic 
 #     download is available, set-up equires data/FCR/data/ to exists
 #-----------------------------------------------------------------------------#
 cd data/FCR/
-if [ -e data ]; then
-    mkdir train val test
-    cd train
-    for f in $(<../train.lst); do
-        ln -s ../data/${f}* .
-    done
+#--- size approx. 1.2GB
+wget https://zenodo.org/record/3945088/files/FCR_500.tar.gz
+tar -xvf FCR_500.tar.gz
+rm FCR_500.tar.gz
+mv FCR_500/data .
 
-    cd ../val
-    for f in $(<../val.lst); do
-        ln -s ../data/${f}* .
-    done
+mkdir train val test
+cd train
+for f in $(<../train.lst); do
+    ln -s ../data/${f}* .
+    ln -s ../data/page/${f}* .
+done
 
-    cd ../test
-    for f in $(<../test.lst); do
-        ln -s ../data/${f}* .
-    done
+cd ../val
+for f in $(<../val.lst); do
+    ln -s ../data/${f}* .
+    ln -s ../data/page/${f}* .
+done
 
-    cd ../../../
-else
-    echo "ERROR: FCR data does not found..."
-    exit
-fi
+cd ../test
+for f in $(<../test.lst); do
+    ln -s ../data/${f}* .
+    ln -s ../data/page/${f}* .
+done
+
+cd ../../../
+exit
 #-----------------------------------------------------------------------------#
 #---- Get ABP dataset
 #-----------------------------------------------------------------------------#
